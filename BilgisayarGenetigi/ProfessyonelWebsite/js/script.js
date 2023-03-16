12099944d7aa0079510b60918357684a31a03a1a
Esra404 <esradrmz571@gmail.com>
2023-03-16 13:48:43 +0300

    responsive not included

---
+++ BilgisayarGenetigi/ProfessyonelWebsite/js/script.js
@@ -0,0 +1,23 @@
+ /*const searchForm=document.querySelector(".search-form");
+ const searchItem=document.querySelector(".cart-items-container");
+ //! buttons
+ const searchBtn=document.querySelector("#search-btn");
+ const cartBtn=document.querySelector("#cart-btn");
+
+
+ searchBtn.addEventListener("click",function(){
+
+ });
+ cartBtn.addEventListener("click",function(){
+    searchItem.classList.toggle("active");
+    
+    document.addEventListener("click",function(e){
+        if(
+            !e.composedPath().includes(cartBtn)&&
+            !e.composedPath().includes(cartItem)
+        ){
+            searchItem.classList.remove("active");
+        }
+    });
+ });
+ //*/
